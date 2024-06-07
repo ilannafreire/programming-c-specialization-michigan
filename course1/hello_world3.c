12d0999304251f76481c0825b030f5a30a90795a
Ilanna Freire <ilannafreiret@hotmail.com>
2024-06-07 13:24:04 -0300

    'Hello word' using individual characters

---
+++ course1/hello_world3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+  char h = 'H';
+  char e = 'e';
+  char l = 'l';
+  char l2 = 'l';
+  char o = 'o';
+  char comma = ',';
+  char space = ' ';
+  char w = 'w';
+  char o2 = 'o';
+  char r = 'r';
+  char l3 = 'l';
+  char d = 'd';
+  char exclamation = '!';
+  char newline = '\n';
+
+  printf("%c%c%c%c%c%c%c%c %c%c%c%c%c%c%c%c%c\n", h, e, l, l2, o, comma, space, w, o2, r, l3, d, exclamation, newline);
+  return 0;
+}
